8a109b39733fa5e8eb5c9ee4d7a7e56ee86405e3
Greg Woodbury <woodbury.greg@gmail.com>
2017-10-05 10:33:57 -0400

    adding auth to user model

--- glycemiq_db/models.py
+++ glycemiq_db/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.Text, nullable=False)
